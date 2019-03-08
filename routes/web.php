0c0890dba02e52716a35e95be9650f58cb9e3f0d
166660614 <166660614@qq.com>
2019-03-08 09:28:55 +0800

    dad

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::post('/weixin/valid', 'UserController@valid');
+Route::get('/weixin/valid', 'UserController@valid');
