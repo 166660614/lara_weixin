2f037147c42ccaeca73b0dacefc923938d2e460b
166660614 <166660614@qq.com>
2019-03-22 16:05:41 +0800

    fsdf

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -17,8 +17,11 @@ class ApiController extends Controller
         $time=time();
         $salt='salt88';
         $iv=substr(md5($time.$salt),5,16);
-        $enc_data=openssl_encrypt($data,$method,$key,$option,$iv);
-        $enc_data=base64_encode($enc_data);
+        $enc_data=base64_encode(openssl_encrypt($data,$method,$key,$option,$iv));
+        $pri_secret=openssl_pkey_get_private(file_get_contents('./priv.key'));
+        openssl_sign($enc_data,$signature,$pri_secret,OPENSSL_ALGO_SHA256);
+        openssl_free_key($pri_secret);//释放资源
+        $sign=base64_encode($signature);
         $curl = curl_init();
         //设置抓取的url
         curl_setopt($curl, CURLOPT_URL, $url);
@@ -31,11 +34,18 @@ class ApiController extends Controller
         //关闭HEADER头
         curl_setopt($curl,CURLOPT_HEADER,0);
         //设置post数据
-        $post_data=['post_data'=>$enc_data];
+        $post_data=['post_data'=>$enc_data,'sign'=>$sign];
         curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
-        $data = curl_exec($curl);
+        $res_data =curl_exec($curl);
+        print_r($res_data);exit;
+        $res_method='AES-128-CBC';
+        $res_salt='salt99';
+        $res_key="reskey";
+        $res_option=OPENSSL_RAW_DATA;
+        $res_iv=substr(md5($res_data['res_time'].$res_salt),5,16);
+        $dec_data=openssl_decrypt($res_data,$res_method,$res_key,$res_option,$res_iv);
+        print_r($dec_data);
         //关闭URL请求
         curl_close($curl);
-        print_r($data);
     }
 }
